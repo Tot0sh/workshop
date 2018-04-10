0fe9d08ae3a37a142a35e2c39932ecd27ec9f61b
Tot0sh <monnier.ru@gmail.com>
2018-04-10 14:12:48 +0200

    login

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,37 +1,34 @@
-				
-<div class="container">
+<section id="section-login">
 
 
-	<div class="card" style="width: 24rem;">
+	<div class="row justify-content-center">
+		<div class="col- col-sm-10 col-md-8 col-lg-6 col-xl-5">
+	
+			<div class="card">
+				<i class="fas fa-user-circle fa-10x"></i>
 
+				<div class="card-body">
 
-		<div class="form-group" >
-			<span class="input-group-text">Login Here</span>
-		</div>
-		<div style="padding: 10px 30px 30px 30px; ">
-			<form action="/aaaa.php">
+						<form action="/aaaa.php">
+							<div class="input-group mb-3">
+								<input type="text" class="form-control" placeholder="Username" aria-label="Recipient's username" aria-describedby="basic-addon2" id="">
+								<div class="input-group-append">
+									<span class="input-group-text" id="basic-addon2"><i class="fas fa-user"></i></span>
+								</div>
+							</div>
 
-				
+							<div class="input-group mb-3">
+								<input type="password" class="form-control" placeholder="Password" aria-label="Recipient's password" aria-describedby="basic-addon2">
+								<div class="input-group-append">
+									<span class="input-group-text" id="basic-addon2"><i class="fas fa-key"></i></span>
+								</div>
+							</div>
 
-				<div class="input-group mb-3">
-					<input type="text" class="form-control" placeholder="Username" aria-label="Recipient's username" aria-describedby="basic-addon2" id="">
-					<div class="input-group-append">
-						<span class="input-group-text" id="basic-addon2"><i class="fas fa-user"></i></span>
-					</div>
+							<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+						</form>	
+					</div>	
 				</div>
 
-				<div class="input-group mb-3">
-					<input type="password" class="form-control" placeholder="Password" aria-label="Recipient's password" aria-describedby="basic-addon2">
-					<div class="input-group-append">
-						<span class="input-group-text" id="basic-addon2"><i class="fas fa-key"></i></span>
-					</div>
-				</div>
-
-				<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
-			</form>
+			</div>
 		</div>
-	</div>
-
-
-</div>
-
+</section>
