b7a12f02052dce4c845048332f9b8b9f6a77418a
ValentinAlvarezEpsi <valentin.alvarez@epsi.fr>
2018-04-10 09:37:47 +0200

    Add bootstrap

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Workshop</title>
-	</head>
-	<body>
-		
-	</body>
-</html>
+<?php
+session_start();
+ 
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=dbtest;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+include('app/views/includes/header.php');
+ 
+if (!empty($_GET['page']) && is_file('controllers/'.$_GET['page'].'.php'))
+{
+    include('app/controllers/'.$_GET['page'].'.php');
+}
+else
+{
+    include('app/controllers/accueil.php');
+}
+ 
+include('app/views/includes/footer.php');
