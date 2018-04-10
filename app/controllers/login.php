9b627b1a634ccc05fa57596c50c189a48b17d394
Tot0sh <monnier.ru@gmail.com>
2018-04-10 12:02:22 +0200

    login

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,50 +1,37 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Workshop</title>
-		<link rel="stylesheet" href="../../css/style.css"/>
-		<script type="text/javascript" src="js/jquery.js"></script>
-	</head>
-	<body>
-		<header>	
+				
+<div class="container">
 
 
-		</header>
+	<div class="card" style="width: 24rem;">
 
 
-
+		<div class="form-group" >
+			<span class="input-group-text">Login Here</span>
+		</div>
+		<div style="padding: 10px 30px 30px 30px; ">
+			<form action="/aaaa.php">
 
 				
-					<div class="container">
 
-
-						<div class="card" style="width: 24rem">
-						<form action="/action_page.php">
-							<div class="form-group" id="pic">
-								
-							</div>
-							<div class="form-group">
-								<input type="email" class="form-control" placeholder="Email address"  id="email">
-							</div>
-							<div class="form-group">
-								<input type="password" class="form-control" placeholder="Password"  id="pwd">
-							</div>
-							<button type="submit" class="btn btn-primary">Connexion</button>
-						</form>
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" placeholder="Username" aria-label="Recipient's username" aria-describedby="basic-addon2" id="">
+					<div class="input-group-append">
+						<span class="input-group-text" id="basic-addon2"><i class="fas fa-user"></i></span>
 					</div>
-
-
 				</div>
 
+				<div class="input-group mb-3">
+					<input type="password" class="form-control" placeholder="Password" aria-label="Recipient's password" aria-describedby="basic-addon2">
+					<div class="input-group-append">
+						<span class="input-group-text" id="basic-addon2"><i class="fas fa-key"></i></span>
+					</div>
+				</div>
+
+				<button type="submit" class="btn btn-primary btn-lg btn-block">Login</button>
+			</form>
+		</div>
+	</div>
 
 
+</div>
 
-
-
-
-
-		<footer>
-		</footer>
-	</body>
-</html>
