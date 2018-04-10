2af22291647ad2a1b45f0d1823487602e326655b
ValentinAlvarezEpsi <valentin.alvarez@epsi.fr>
2018-04-10 14:12:51 +0200

    Add accueil

--- app/views/accueil.php
+++ app/views/accueil.php
@@ -2,14 +2,24 @@
 	
 
 
-	<div class="test">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	<div class="row">
+		<div class="col-sm">
+
+			<div class="card">
+				<div class="card-header">Accueil</div>
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+
+		</div>
+
 	</div>
+
+
+	
 
 
 
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -7,12 +7,35 @@
 		<script type="text/javascript" src="js/jquery.js"></script>
 	</head>
 	<body>
-		<header>
-			<nav class="navbar navbar-light bg-light">
+		<header class="mb-5">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
 				<a class="navbar-brand" href="?page=accueil">
-					<img src="images/logo.png"  class="d-inline-block align-top"/>
+					<img src="images/logo-mini.svg" height="30" width="30" class="d-inline-block align-top mr-2"/>
 					Workshop
 				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarText">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link <?php if($_GET['page'] == 'accueil') echo ' disabled'; ?>" href="?page=accueil">Accueil <span class="sr-only">(current)</span></a>
+						</li>
+					</ul>
+
+					<div class="btn-group mr-3" role="group" aria-label="Basic example">
+						<button type="button" class="btn">
+							<i class="fas fa-user"></i>
+						</button>
+						<button type="button" class="btn">
+							<i class="fas fa-cog fa-lg"></i>
+						</button>
+					</div>
+
+					<button class="btn btn-outline-danger">
+						<i class="fas fa-sign-out-alt fa-lg"></i>
+					</button>
+				</div>
 			</nav>
 		</header>
 
