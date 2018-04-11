8fc5aedf678958442d7d02a95954d1cf98e954e9
Tot0sh <monnier.ru@gmail.com>
2018-04-11 23:45:18 +0200

    Add files via upload

--- app/views/login.php
+++ app/views/login.php
@@ -1,56 +1,38 @@
 <section id="section-login">
 
 	<div class="row justify-content-center">
-		<div class="col- col-sm-10 col-md-8 col-lg-6 col-xl-4">
-			<div class="card">
+		<div class="col-10 col-sm-10 col-md-8 col-lg-6 col-xl-5">
 
+			<div class="card">
 				<div class="card-header">
 					<i class="fas fa-sign-in-alt"></i> Connexion
 				</div>
-				
-				<div class="card-body">
 
+				<div class="card-body" >
 					<div class="row justify-content-center mb-4">
 						<i class="fas fa-user-circle fa-10x"></i>
 					</div>
 
-					<?php if(isset($_GET["statut"])): ?>
-
-
-						<?php if($_GET["statut"] == 'apprenant' || $_GET["statut"] == 'intervenant' || $_GET["statut"] == 'responsable'): ?>
-
-							<form action="?page=login" method="post" autocomplete="off">
-								<div class="input-group mb-3">
-									<input type="lastname" id="groupName" name="lastname" value="<?php if(isset($lastname)) echo $lastname; ?>" class="form-control" placeholder="Nom" aria-describedby="basic-addon"/>
-									<div class="input-group-append">
-										<span class="input-group-text" id="basic-addon"><i class="fas fa-user"></i></span>
-									</div>
-								</div>
-								<div class="input-group mb-3">
-									<input type="password" id="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon2"/>
-									<div class="input-group-append">
-										<span class="input-group-text" id="basic-addon2"><i class="fas fa-key"></i></span>
-									</div>
-								</div>
-								<button type="submit" id="submit" name="submit" class="btn btn-primary btn-block">Login</i></button>
-							</form>
-
-							<?php if(isset($error) && $error): ?>
-								<div class="alert alert-danger m-0 mt-3" role="alert">
-									<strong>Erreur :</strong> Compte inconnu
-								</div>
-							<?php endif; ?>
-
-						<?php endif; ?>
-
-					<?php else: ?>
-
-						<div class="list-group">
-							<a href="index.php?page=login&statut=apprenant" class="list-group-item list-group-item-action">Apprenant</a>
-							<a href="index.php?page=login&statut=intervenant" class="list-group-item list-group-item-action">Intervenant</a>
-							<a href="index.php?page=login&statut=responsable" class="list-group-item list-group-item-action">Responsable</a>
+					<form action="?page=login" method="post" autocomplete="off">
+						<div class="input-group mb-3">
+							<input type="text" id="groupName" name="login" value="<?php if(isset($login)) echo $login; ?>" class="form-control" placeholder="Login" aria-describedby="basic-addon"/>
+							<div class="input-group-append">
+								<span class="input-group-text" id="basic-addon"><i class="fas fa-user"></i></span>
+							</div>
 						</div>
+						<div class="input-group mb-3">
+							<input type="password" id="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon2"/>
+							<div class="input-group-append">
+								<span class="input-group-text" id="basic-addon2"><i class="fas fa-key"></i></span>
+							</div>
+						</div>
+						<button type="submit" id="submit" name="submit" class="btn btn-primary btn-block">Login</i></button>
+					</form>
 
+					<?php if(isset($error) && $error): ?>
+						<div class="alert alert-danger m-0 mt-3" role="alert">
+					  		<strong>Erreur :</strong> Compte inconnu
+						</div>
 					<?php endif; ?>
 
 				</div>	
@@ -59,3 +41,8 @@
 	</div>
 
 </section>
+
+<script type="text/javascript">
+	
+
+</script>
